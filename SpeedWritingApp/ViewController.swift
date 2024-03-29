3e902f98c22da25fb8bb6c097421cf8155b12047
KJNyhlin <karlnyhlin@me.com>
2024-03-29 20:51:23 +0100

    Initial Commit

---
+++ SpeedWritingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeedWritingApp
+//
+//  Created by Karl Nyhlin on 2024-03-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
